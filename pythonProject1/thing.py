239d82df301a8c345b4d6556854d402bb25c6df0
awidmer7246 <awidmer7246@student.pps.net>
2024-06-11 13:39:11 -0700

    unfinished

--- pythonProject1/thing.py
+++ pythonProject1/thing.py
@@ -28,7 +28,7 @@ def update():
     event = pygame.event.get()
     mx,my = pygame.mouse.get_pos()
     player.scale = 1
-
+    print(zombie.x)
     if keys[pygame.K_LEFT]:
         if player.x > 0:
             player.x -= 5
